b9cf9ce2ec7a5429a5ad80ba38759ec930ee27eb
ZainA11 <zin.ala880@gmail.com>
2024-01-25 21:00:22 +0000

    wrote functions to get time in unix and utc

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ let express = require('express');
 let app = express();
 let port = 3000;
 
-app.get('/', (req, res) =>{
-  res.send('connection done');
+app.get('/', function(req, res){
+  res.send('Hello World!');
+});
+
+app.get('/api:date', (req, res) =>{
+  let date = req.params.date;
+  let unix = new Date(date).getTime();
+  let utc = new Date(date).toUTCString();
+  res.json({unix: unix, utc: utc});
 });
 
 app.listen(port, () => {
